a0579de318a6cdadcee1be1615e5b960b4aca9ce
Colin Keenan <colinnkeenan@gmail.com>
2012-12-07 22:43:58 -0600

    Started dwellings/models.py: Owner, LandTransfer

--- dwellings/models.py
+++ dwellings/models.py
@@ -1,3 +1,21 @@
+import people, places, datetime
+import datetime
+from django.utils import timezone
+from django import forms
+from django_localflavor_us.forms import USPhoneNumberField, USPSSelect, USSocialSecurityNumberField, USZipCodeField
+from django_localflavor_us.models import PhoneNumberField, USPostalCodeField # two-letter postal codes: state/territory/country
 from django.db import models
 
-# Create your models here.
+class LandTransfers(models.Model):
+    owner = models.ForeignKey(Owner)
+    land = models.ForeignKey(Land)
+    date = models.DateField('sale date')
+    price = models.DecimalField('sale price', max_digits=14, decimal_places=2)
+
+class Owner(models.Model):
+    birth = models.ForeignKey(people.Birth)
+    lands = models.ManyToManyField(places.Land, through = 'LandTransfers')
+
+    def isLandlord(self):
+        """ returns True if self has rental units """
+        return self.rental_unit_set.exists()
